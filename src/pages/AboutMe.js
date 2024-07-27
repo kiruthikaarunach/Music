91f699dcac4bf855cf062873a3de5200bf4e213e
Kiruthika Arunachalam <kiruthika17.arun@gmail.com>
2024-07-27 17:37:22 -0400

    prod-testing

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -81,16 +81,16 @@ const AboutMe = () => {
         <div className="guru-section">
           {/* Left column for descriptions */}
           <div className="guru-column">
-            <div className="right-column">
+          <div className="centered-text">
+
               <i className="fas fa-music"></i>
               She is part of Music Director James Vasanthan's Tamizhosai team and has performed in many shows in India and Canada in his choir. Tamizhosai is a 30-voice Choral Ensemble which sings ancient Tamil poems from Sangam Literature to convey the classical literature to the present generation through popular music.
               <i className="fas fa-heart"></i>
             </div>
-          </div>
+            </div>
 
           {/* Right column for photos and video */}
           <div className="guru-column">
-            <div className="left-column">
               <img
                 src={`${process.env.PUBLIC_URL}/Tamizhosai.jpg`}
                 alt="Guru 1"
@@ -111,7 +111,6 @@ const AboutMe = () => {
                   allowFullScreen
                 ></iframe>
               </div>
-            </div>
           </div>
         </div>
 
@@ -119,22 +118,21 @@ const AboutMe = () => {
         <div className="guru-section">
           {/* Left column for photos */}
           <div className="guru-column">
-            <div className="left-column">
               <img
                 src={`${process.env.PUBLIC_URL}/Vandana-Srinivasan.jpg`}
                 alt="Guru 1"
                 className="guru-image"
               />
-            </div>
           </div>
 
           {/* Right column for descriptions */}
           <div className="guru-column">
-            <div className="right-column">
+          <div className="centered-text">
+
               <i className="fas fa-music"></i>
               She is learning semi-classical music from famous playback singer Vandana Srinivasan who is an Indian playback singer, working mainly for the South Indian film industry especially in Tamil, Telugu, and Kannada.
               <i className="fas fa-heart"></i>
-            </div>
+          </div>
           </div>
         </div>
 
@@ -142,22 +140,20 @@ const AboutMe = () => {
         <div className="guru-section">
           {/* Left column for descriptions */}
           <div className="guru-column">
-            <div className="right-column">
+          <div className="centered-text">
+
               <i className="fas fa-music"></i>
               She learns film music from famous Vijay TV Super Singer and Jaya Super Singer Winner and Playback Singer Keshav Vinod.
               <i className="fas fa-heart"></i>
-            </div>
           </div>
-
+</div>
           {/* Right column for photos */}
           <div className="guru-column">
-            <div className="left-column">
               <img
                 src={`${process.env.PUBLIC_URL}/keshav-vinod.jpg`}
                 alt="Guru 2"
                 className="guru-image"
               />
-            </div>
           </div>
         </div>
     </Box>
